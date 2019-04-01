0ddf42bc62aa70f1bf4e5a0cdb1aa395ae2fdb49
= <=>
2019-04-01 15:24:18 +0200

    salut

---
+++ src/com/model/User.java
@@ -0,0 +1,10 @@
+package com.model;
+
+
+public  abstract class User {
+    protected int id;
+    protected String firstName;
+    protected String lastName;
+    protected String login;
+    protected String password;
+}
